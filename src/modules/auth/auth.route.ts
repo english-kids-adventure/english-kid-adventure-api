882185896535f16b91dd9f60046d641b605aa70e
Hanh_Dev Clean <hanh.ho26@student.passerellesnumeriques.org>
2025-12-23 23:32:54 +0700

    VLIST-43 | Add validation middleware

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { AuthController } from './auth.controller';
+import { validate } from '../../middlewares/validate';
+import { RegisterSchema } from './auth.schema';
 
 const authRoutes: Router = Router();
 
-authRoutes.post('/register', AuthController.register);
+authRoutes.post('/register', validate(RegisterSchema), AuthController.register);
 export default authRoutes;
--- src/modules/auth/auth.schema.ts
+++ src/modules/auth/auth.schema.ts
@@ -1,7 +1,15 @@
 import z from 'zod';
 
 export const RegisterSchema = z.object({
-  name: z.string('Name is required!').min(2),
-  email: z.string('Email is required!'),
-  password: z.string('Password is required!').min(6),
+  name: z
+    .string({ error: 'Name is required' })
+    .min(2, 'Name must be at least 2 characters'),
+
+  email: z
+    .string({ error: 'Email is required' })
+    .email('Invalid email address'),
+
+  password: z
+    .string({ error: 'Password is required' })
+    .min(6, 'Password must be at least 6 characters'),
 });
