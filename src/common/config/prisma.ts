51491add7c9889be4fac3a82e914495f365a3523
Hanh_Dev Clean <hanh.ho26@student.passerellesnumeriques.org>
2025-12-30 11:24:56 +0700

    VLIST-100 | Add deploy pipeline

--- src/common/config/prisma.ts
+++ src/common/config/prisma.ts
@@ -1,10 +1,25 @@
 import 'dotenv/config';
+import fs from 'fs';
+import path from 'path';
+import pg from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../../generated/prisma/client';
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const caPath = path.resolve(process.cwd(), './src/common/config/ca.pem');
 
-const adapter = new PrismaPg({ connectionString });
+const pool = new pg.Pool({
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  database: process.env.DATABASE_NAME,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(caPath).toString(),
+  },
+});
+
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
