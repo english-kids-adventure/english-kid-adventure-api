a614543da0d60ab053481d210337b96e8b503735
Hanh_Dev Clean <hanh.ho26@student.passerellesnumeriques.org>
2025-12-30 21:50:39 +0700

    VLIST-100 | Update pipeline

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use('/api/v1/auth', authRoutes);
 app.get('/health', (req, res) => {
-  res.status(200).send('OK');
+  res.status(200).send('OK 🚀');
 });
 app.use('/api/v1/topics', topicRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
